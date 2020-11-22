951aa35ac81008a66aba9e64b3a823cd0ee1bc5d
Heiner Peuser <heiner@MacBook.local>
2020-11-22 20:09:35 +0100

    Added language detection

--- types/i18n.ts
+++ types/i18n.ts
@@ -1,6 +1,23 @@
 import i18n from 'i18next';
+//import { LanguageDetectorAsyncModule, InitOptions, Services } from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import * as Localization from 'expo-localization';
 
+/*
+console.log("test 1");
+class LanguageDetector implements LanguageDetectorAsyncModule {
+    type: 'languageDetector' = 'languageDetector';
+    async: true = true;
+    detect = (callback: any) => {
+        console.log("----> " + Localization.locale.split('-')[0]);
+        callback("en");
+    };
+    init = (services: Services, detectorOptions: object, i18nextOptions: InitOptions) => {};
+    cacheUserLanguage = (lng: string) => {};
+}
+const languageDetector = new LanguageDetector();
+*/
+const deviceLanguage = Localization.locale.split('-')[0];
 const resources = {
     de: {
         translation: {
@@ -81,10 +98,11 @@ const resources = {
 };
 
 i18n
+//.use(languageDetector)
 .use(initReactI18next)
 .init({
     resources,
-    lng: "de",
+    lng: deviceLanguage,
     fallbackLng: "en",
     keySeparator: false
 });
